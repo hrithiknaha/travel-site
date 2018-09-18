599b337d991858abf625ea0007d8eb4f79e3e611
Hrithik Naha <naha.hrithkk@gmail.com>
2018-09-18 13:37:31 +0530

    error RECOVERY

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,7 +1,9 @@
 import MobileMenu from './Modules/MobileMenu';
 import RevealOnScroll from './Modules/RevealOnScroll';
+import StickyHeader from './Modules/StickyHeader';
 import $ from 'jquery';
 
 var mobileMenu = new MobileMenu(); 
 new RevealOnScroll($(".feature-item"), "85%");
 new RevealOnScroll($(".testimonial"), "60%");
+var stickyHeader = new StickyHeader();
