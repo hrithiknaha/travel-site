3157005ec58790180273da6d62db831f283c9571
Hrithik Naha <naha.hrithkk@gmail.com>
2018-09-18 14:26:00 +0530

    modal completed

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,9 +1,11 @@
 import MobileMenu from './Modules/MobileMenu';
 import RevealOnScroll from './Modules/RevealOnScroll';
 import StickyHeader from './Modules/StickyHeader';
+import Modal from './Modules/Modal';
 import $ from 'jquery';
 
 var mobileMenu = new MobileMenu(); 
 new RevealOnScroll($(".feature-item"), "85%");
 new RevealOnScroll($(".testimonial"), "60%");
 var stickyHeader = new StickyHeader();
+var modal = new Modal();
